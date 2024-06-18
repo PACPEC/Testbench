a758df641fcf3e252265dabd72912e23ed5a7312
andmas199 <andreivc7121@gmail.com>
2024-06-18 20:55:50 +0200

    Adapt div test to SISA-F

--- div_fp_test.s
+++ div_fp_test.s
@@ -1,63 +1,54 @@
-MOVI R1, 16
+.include "macros.s"
 
-$MOVIF F1, 0x3E48
-$MOVIF F2, 0x4361
+MOVI R1, 0x10
+
+$MOVIF F1, 0x3F23
+$MOVIF F2, 0x41B0
 
 ; 1.57 / 3.69
 DIVF F3, F1, F2 ; Division dos numeros normales pequeños
 STF 0(R1), F3
 
-$MOVIF F1, 0x7104
-$MOVIF F2, 0x7732
+$MOVIF F1, 0x5881
+$MOVIF F2, 0x5B98
 
 ; 10268.786 / 29468.352
 DIVF F3, F1, F2 ; Division dos numeros normales grandes
 STF 2(R1), F3
 
-$MOVIF F1, 0x7732
-$MOVIF F2, 0x4361
+$MOVIF F1, 0x5B98
+$MOVIF F2, 0x41B0
 
+; 3.69 / 29468.352
 DIVF F3, F1, F2 ; Division dos numeros normales grandes y pequeños
 STF 4(R1), F3
 
-$MOVIF F1, 0x0001
-$MOVIF F2, 0x03ff
+$MOVIF F1, 0x7DFF
+$MOVIF F2, 0x0200
 
-;0.000000059604645 / 0.000060975552
-DIVF F3, F1, F2 ; Division dos numeros denormales diferentes
+; 4290772992 / 0.000000000931322
+DIVF F3, F1, F2 ; Division numero mas grande entre mas pequeño (overflow)
 STF 6(R1), F3
 
-DIVF F3, F1, F1 ; Division dos numeros denormales iguales
+$MOVIF F1, 0x4066
+$MOVIF F2, 0xC066
+
+; 2.5 / -2.5
+DIVF F3, F1, F2 ; Division + / -
 STF 8(R1), F3
 
-$MOVIF F1, 0x4361
-$MOVIF F2, 0x7C01
-
-DIVF F3, F1, F2 ; Division numero / NaN
+; -2.5 / 2.5
+DIVF F3, F2, F1 ; Division - / +
 STF 10(R1), F3
 
-DIVF F3, F2, F1 ; Division NaN / numero
+; -2.5 / -2.5
+DIVF F3, F2, F2 ; Division - / -
 STF 12(R1), F3
-
-DIVF F3, F2, F2 ; Division dos NaN
-STF 14(R1), F3
-
-$MOVIF F1, 0x4361
-$MOVIF F2, 0x7C00
-
-DIVF F3, F1, F2 ; Division numero / inf
-STF 16(R1), F3
-
-DIVF F3, F2, F1 ; Division inf / numero
-STF 18(R1), F3
-
-DIVF F3, F2, F2 ; Division inf / inf
-STF 20(R1), F3
 
 $MOVIF F2, 0x0000
 
 DIVF F3, F1, F2 ; Division entre 0
-STF 24(R1), F3
+STF 14(R1), F3
 
 DIVF F3, F2, F2 ; Division 0 / 0
-STF 26(R1), F3
+STF 16(R1), F3
