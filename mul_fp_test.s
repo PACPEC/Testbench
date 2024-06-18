199f158442d5531b55ba79d0e1d2e4e4bdd73b1d
andmas199 <andreivc7121@gmail.com>
2024-06-18 20:39:59 +0200

    Fix mul test

--- mul_fp_test.s
+++ mul_fp_test.s
@@ -9,17 +9,17 @@ $MOVIF F2, 0x41B0
 MULF F3, F1, F2 ; Multiplicacion dos numeros normales pequeños
 STF 0(R1), F3
 
-$MOVIF F1, 0x3E48
-$MOVIF F2, 0x7104
+$MOVIF F1, 0x41B0
+$MOVIF F2, 0x5881
 
 ;3.69 * 10268.786
 MULF F3, F1, F2 ; Multiplicacion dos numeros normales pequeños y grandes (no overflow)
 STF 2(R1), F3
 
-$MOVIF F1, 0x7104
-$MOVIF F2, 0x7732
+$MOVIF F1, 0x7DFF
+$MOVIF F2, 0x5B98
 
-; 10268.786 * 29468.352
+; 4290772992 * 29468.352
 MULF F3, F1, F2 ; Multiplicacion dos numeros normales grandes (overflow)
 STF 4(R1), F3
 
@@ -32,11 +32,14 @@ STF 6(R1), F3
 $MOVIF F1, 0x4066
 $MOVIF F2, 0xC066
 
+; 2.5 * -2.5
 MULF F3, F1, F2 ; Multiplicacion + * -
 STF 8(R1), F3
 
+; -2.5 * 2.5
 MULF F3, F2, F1 ; Multiplicacion - * +
 STF 10(R1), F3
 
+; -2.5 * -2.5
 MULF F3, F2, F2 ; Multiplicacion - * -
 STF 12(R1), F3
