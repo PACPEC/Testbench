f413d19de9b28f08fdce355a9fc7dda88fa5f155
andmas199 <andreivc7121@gmail.com>
2024-06-18 20:23:33 +0200

    Add new cases to sub test

--- sub_fp_test.s
+++ sub_fp_test.s
@@ -33,3 +33,16 @@ $MOVIF F2, 0xD735
 ; -4530.36 - (-6571.36)
 SUBF F3, F1, F2 ; Resta dos numeros negativos
 STF 8(R1), F3
+
+$MOVIF F1, 0x0300
+$MOVIF F2, 0x0200
+
+
+SUBF F3, F1, F2 ; Resta dos numeros negativos (underflow)
+STF 10(R1), F3
+
+$MOVIF F1, 0x7DFF
+$MOVIF F2, 0xFDFF
+
+SUBF F3, F1, F2 ; Resta dos numeros (overflow)
+STF 12(R1), F3
